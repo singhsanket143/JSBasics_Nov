8295950fd78debe4b2684ab5d8378bec4ca5d04c
Sanket Singh <sanketsingh@Sankets-MBP.bbrouter>
2022-11-11 21:39:35 +0530

    Added codes

---
+++ Nov_11/specialchars.js
@@ -0,0 +1,8 @@
+let text = "The new apple iphone \n has been launched";
+console.log(text);
+let text2 = "The new apple iphone 14 \t has been launched";
+console.log(text2);
+
+let age = 10;
+let name = "Sanket";
+console.log(name, "\n", age);
