38cfb0c7eeac60cc384de1677ba8459c66d3faee
Sanket Singh <singhsanket143@gmail.com>
2022-12-10 21:50:10 +0530

    Added codes

--- Dec_10/twosum.js
+++ Dec_10/twosum.js
@@ -1,15 +1,23 @@
 /**
  * https://leetcode.com/problems/two-sum/description/
- * @param {number[]} numbers
+ * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
+function fn(x, y) {
+    return x[0] - y[0];
+}
 var twoSum = function(arr, target) {
+    for(let i = 0; i < arr.length; i++) {
+        arr[i] = [arr[i], i];
+    }
+    // console.log(arr);
+    arr.sort(fn);
     let start = 0, end = arr.length - 1; 
     while(start < end) {
-        if(arr[start] + arr[end] == target) {
-            return [start + 1, end + 1];
-        } else if (arr[start] + arr[end] > target) {
+        if(arr[start][0] + arr[end][0] == target) {
+            return [arr[start][1], arr[end][1]];
+        } else if (arr[start][0] + arr[end][0] > target) {
             end--;
         } else {
             start++;
